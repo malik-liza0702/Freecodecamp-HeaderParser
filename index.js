d677f1f6959e06396b79dc16db7a8cbcbbccdaa3
malik-liza <malik.liza0702@gmail.com>
2024-07-08 10:20:09 +0530

    first

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami",(req,res)=>{
+  res.json({
+    ipaddress:"159.20.14.100",
+    language:"en-US,en;q=0.5",
+    software:"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+
+  })
+})
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
